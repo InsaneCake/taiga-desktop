f6b8b029247ce6c709e018ed97ac20ff0d572fa0
AntonChubov <AntonChubov@gmail.com>
2022-01-13 00:32:14 +0300

    Initial project template Vue3 electron 13 updated to 16

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # taiga-desktop
-Desktop application for taiga.io - open source project management tool
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo-color.png">
+  <HelloWorld msg="Welcome to future Taiga Desktop App" submsg="Vue3 + Electron16"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
